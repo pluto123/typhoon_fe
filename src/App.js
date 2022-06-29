b81bf6d0904f26c7b1f2b79089763c6d2a00e729
Steven Wu <stevenwu@mirle.com.tw>
2022-06-29 16:44:14 +0800

    add dashboard page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import NotFound from "./components/NotFound";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<SignIn />} />
         <Route path='/SignIn' element={<SignIn />} />
         <Route path='/SignUp' element={<SignUp />} />
+        <Route path='/Dashboard' element={<Dashboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
